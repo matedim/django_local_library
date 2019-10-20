c54b9b8fc213e1da68c6f25c81a7acc0612ecd00
Matedim <dmitrikzp@gmail.com>
2019-10-20 15:37:31 +0300

    Update

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,7 +7,7 @@ from blog.sitemaps import PostSitemap
 sitemaps = {'posts': PostSitemap,}
 
 urlpatterns = [
-    path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls', namespace='blog')),
     path('admin/', admin.site.urls),
     path('sitemap.xml', sitemap, {'sitemaps':sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
